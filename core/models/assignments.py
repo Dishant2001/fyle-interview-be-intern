dd96b53b1a9da6bb23fbb7b08dc8a2e65624712d
Dishant <dishant.rathee@gmail.com>
2021-10-08 12:04:13 +0530

    pushing again, interruption caused previous time

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -75,5 +75,25 @@ class Assignment(db.Model):
         return assignment
 
     @classmethod
+    def grading(cls,_id,grade,principal:Principal):
+        print("Entered grading")
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.student_id == principal.user_id, 'This assignment belongs to some other student')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,
+                                'only a draft assignment can be submitted')
+        assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+
+        assignment.grade = grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def get_assignments_for_teacher(cls,teacher_id):
+        return cls.filter(cls.teacher_id==teacher_id).all()
